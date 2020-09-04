59b66a96aca6a8074bae002d3c52b9a16e640653
Lucas <lucasbmat@gmail.com>
2020-09-04 09:48:23 -0300

    add Example app

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Lucas Bitar Matos on 04/09/20.
+//  Copyright © 2020 bitlucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
